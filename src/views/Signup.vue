52384a42c12872da0d19ffd133faddca1a2b7e5e
Eduardo Affonso <Eduardoaffonsoz@hotmail.com>
2025-10-24 23:35:11 -0300

    Corrigir cadastro

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -22,30 +22,30 @@
 		</div>
   
 		<form @submit.prevent="handleRegister" class="flex flex-col gap-4">
-		  <input v-model="form.name" type="text" placeholder="Name" class="input" />
-		  <input v-model="form.email" type="email" placeholder="Email" class="input" />
+		  <input v-model="form.name" type="text" placeholder="Nome completo" class="input" />
+		  <input v-model="form.email" type="email" placeholder="E-mail" class="input" />
   
 		  <input v-if="userType === 'person'" v-model="form.cpf" type="text" placeholder="CPF" class="input" />
 		  <input v-else v-model="form.cnpj" type="text" placeholder="CNPJ" class="input" />
   
-		  <input v-model="form.password" type="password" placeholder="Password" class="input" />
-		  <input v-model="form.confirmPassword" type="password" placeholder="Confirm Password" class="input" />
+		  <input v-model="form.password" type="password" placeholder="Senha" class="input" />
+		  <input v-model="form.confirmPassword" type="password" placeholder="Confirmar senha" class="input" />
   
-		  <input v-if="userType === 'person'" v-model="form.birthday" type="date" placeholder="Birthday" class="input" />
+		  <input v-if="userType === 'person'" v-model="form.birthday" type="date" placeholder="Data de nascimento" class="input" />
   
-		  <input v-model="form.street" type="text" placeholder="Street" class="input" />
+		  <input v-model="form.street" type="text" placeholder="Rua" class="input" />
 		  <div class="flex gap-2">
-			<input v-model="form.number" type="text" placeholder="Number" class="input flex-1" />
-			<input v-model="form.complement" type="text" placeholder="Complement" class="input flex-1" />
+			<input v-model="form.number" type="text" placeholder="Número" class="input flex-1" />
+			<input v-model="form.complement" type="text" placeholder="Complemento" class="input flex-1" />
 		  </div>
-		  <input v-model="form.neighborhood" type="text" placeholder="Neighborhood" class="input" />
-		  <input v-model="form.city" type="text" placeholder="City" class="input" />
-		  <input v-model="form.zipCode" type="text" placeholder="Zip Code" class="input" />
-		  <input v-model="form.state" type="text" placeholder="State" class="input" />
+		  <input v-model="form.neighborhood" type="text" placeholder="Bairro" class="input" />
+		  <input v-model="form.city" type="text" placeholder="Cidade" class="input" />
+		  <input v-model="form.zipCode" type="text" placeholder="CEP" class="input" />
+		  <input v-model="form.state" type="text" placeholder="Estado" class="input" />
   
 		  <div class="flex gap-2">
-			<input v-model="form.areaCode" type="text" placeholder="Area Code" class="input w-1/5 max-w-1/5" />
-			<input v-model="form.phoneNumber" type="text" placeholder="Phone Number" class="input" />
+			<input v-model="form.areaCode" type="text" placeholder="DDD" class="input w-1/5 max-w-1/5" />
+			<input v-model="form.phoneNumber" type="text" placeholder="Telefone" class="input" />
 		  </div>
   
 		  <button type="submit" class="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 rounded font-medium">
@@ -92,57 +92,57 @@
 	  };
 	},
 	methods: {
-		async handleRegister() {
+	  async handleRegister() {
 		try {
-			const payload =
-				this.userType === "person"
-					? {
-						name: this.form.name,
-						email: this.form.email,
-						cpf: this.form.cpf,
-						password: this.form.password,
-						confirmPassword: this.form.confirmPassword,
-						birthday: this.form.birthday,
-						street: this.form.street,
-						number: this.form.number,
-						complement: this.form.complement,
-						neighborhood: this.form.neighborhood,
-						city: this.form.city,
-						zipCode: this.form.zipCode,
-						state: this.form.state,
-						phoneNumber: this.form.phoneNumber,
-						areaCode: this.form.areaCode,
-					}
-					: {
-						name: this.form.name,
-						email: this.form.email,
-						cnpj: this.form.cnpj,
-						password: this.form.password,
-						confirmPassword: this.form.confirmPassword,
-						street: this.form.street,
-						number: this.form.number,
-						complement: this.form.complement,
-						neighborhood: this.form.neighborhood,
-						city: this.form.city,
-						zipCode: this.form.zipCode,
-						state: this.form.state,
-						phoneNumber: this.form.phoneNumber,
-						areaCode: this.form.areaCode,
-					};
-
-			const url =
+		  const payload =
 			this.userType === "person"
-				? "http://localhost:4200/api/user"
-				: "http://localhost:4200/api/company";
-
-			const response = await axios.post(url, payload);
-
-			this.router.push("/login");
+			  ? {
+				  name: this.form.name,
+				  email: this.form.email,
+				  cpf: this.form.cpf,
+				  password: this.form.password,
+				  confirmPassword: this.form.confirmPassword,
+				  birthday: this.form.birthday,
+				  street: this.form.street,
+				  number: this.form.number,
+				  complement: this.form.complement,
+				  neighborhood: this.form.neighborhood,
+				  city: this.form.city,
+				  zipCode: this.form.zipCode,
+				  state: this.form.state,
+				  phoneNumber: this.form.phoneNumber,
+				  areaCode: this.form.areaCode,
+				}
+			  : {
+				  name: this.form.name,
+				  email: this.form.email,
+				  cnpj: this.form.cnpj,
+				  password: this.form.password,
+				  confirmPassword: this.form.confirmPassword,
+				  street: this.form.street,
+				  number: this.form.number,
+				  complement: this.form.complement,
+				  neighborhood: this.form.neighborhood,
+				  city: this.form.city,
+				  zipCode: this.form.zipCode,
+				  state: this.form.state,
+				  phoneNumber: this.form.phoneNumber,
+				  areaCode: this.form.areaCode,
+				};
+  
+		  const url =
+			this.userType === "person"
+			  ? "http://localhost:4200/api/user"
+			  : "http://localhost:4200/api/company";
+  
+		  await axios.post(url, payload);
+  
+		  this.router.push("/login");
 		} catch (error) {
-			console.error("Erro ao cadastrar:", error);
-			alert("Erro ao cadastrar. Verifique os dados e tente novamente.");
+		  console.error("Erro ao cadastrar:", error);
+		  alert("Erro ao cadastrar. Verifique os dados e tente novamente.");
 		}
-	}
+	  },
 	},
   };
   </script>
